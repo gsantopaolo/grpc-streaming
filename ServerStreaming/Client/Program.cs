c5b26407348a3f2ef882019c3cf833bbd07f486a
Gian Paolo Santopaolo <GianPaolo.Santopaolo@ibv.ch>
2020-09-15 15:50:15 +0200

    working on samples

---
+++ ServerStreaming/Client/Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Sensorsystem;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new SensorService.SensorServiceClient(channel);
+
+            //Task unarySend = SendUnaryDataAasync(client);
+            //Task clientStreaming = SendStremingDataAasync(client);
+            Task serverStreaming = ReceiveStreamingDataAsync(client, 1);
+
+            //await Task.WhenAll(unarySend, clientStreaming, serverStreaming);
+
+            await Task.WhenAll(serverStreaming);
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task SendUnaryDataAasync(SensorService.SensorServiceClient client)
+        {
+            await Task.Delay(2000);
+            var reply = await client.GetAvailableSensorsAsync(new Sensorsystem.AvailableSensorsRequest { Username = "username", Message = "message from client", });
+            Console.WriteLine($"Server response: { reply.Message }");
+            Console.WriteLine($"Devices from server: { reply.Devices }");
+        }
+
+        //client streaming
+        private static async Task SendStremingDataAasync(SensorService.SensorServiceClient client)
+        {
+            using var call = client.SendSensorData();
+            for (var i = 0; i < 100; i++)
+            {
+                await call.RequestStream.WriteAsync(new SensorData { Data1 = $"Message{i}, Data2 = {i}" });
+                await Task.Delay(200);
+            }
+
+            await call.RequestStream.CompleteAsync();
+
+            var response = await call;
+            Console.WriteLine($"Message response: {response.Message}");
+        }
+
+        //server streaming
+        private static async Task ReceiveStreamingDataAsync(SensorService.SensorServiceClient client, int deviceId)
+        {
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(5.0));
+
+            using (var call = client.SendTemperatureUpdates(new TemperatureRequest { Deviceid = deviceId }, cancellationToken: cts.Token))
+            {
+                try
+                {
+                    await foreach (var message in call.ResponseStream.ReadAllAsync())
+                    {
+                        Console.WriteLine($"Temperature for device located at: {message.Devicelocation} is {message.Temperature}");
+                    }
+                }
+                catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+                {
+                    Console.WriteLine("Stream canceled.");
+                }
+            }
+        }
+    }
+}
---
+++ ServerStreaming/Server/Services/SensorHub.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Sensorsystem;
+
+namespace Server
+{
+    public class SensorHub : SensorService.SensorServiceBase
+    {
+        private readonly ILogger<SensorHub> _logger;
+        private static Random randomizer = new Random();
+        public SensorHub(ILogger<SensorHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AvailableSensorsResponse> GetAvailableSensors(AvailableSensorsRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"received a GetAvailableSensors request : {request.Message}");
+
+            return Task.FromResult(new AvailableSensorsResponse
+            {
+                Message = "Hello from server",
+                Devices = "This is the server replying with a list of devices"
+            });
+        }
+
+        public override async Task<SensorDataResponse> SendSensorData(IAsyncStreamReader<SensorData> requestStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"received some data from client Data1: {message.Data1}, Data2: {message.Data2}");
+            }
+
+            return new SensorDataResponse { Message = "Streamed data received correctly" };
+        }
+
+        public override async Task<Empty> SendSensorDataNoResponse(IAsyncStreamReader<SensorData> requestStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"received some data from client Data1: {message.Data1}, Data2: {message.Data2}");
+            }
+
+            return new Empty();
+        }
+
+        public override async Task SendTemperatureUpdates(TemperatureRequest request, IServerStreamWriter<TemperatureData> responseStream, ServerCallContext context)
+        {
+            _logger.LogInformation($"received request to start stream temperature data from device id: {request.Deviceid}");
+
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Sending temperature data to device id: {request.Deviceid}.");
+
+                await responseStream.WriteAsync(new TemperatureData { Devicelocation = "", Temperature = randomizer.Next(1, 100)});
+
+                await Task.Delay(500);
+            }
+
+            _logger.LogInformation($"stop streaming temperature data to device id: {request.Deviceid}");
+        }
+    }
+}
