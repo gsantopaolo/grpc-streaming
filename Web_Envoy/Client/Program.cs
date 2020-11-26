bb14d4c61886b1dd5c48b78cce3d6d661c4fcdaf
Gian Paolo Santopaolo <GianPaolo.Santopaolo@ibv.ch>
2020-11-26 12:19:59 +0100

    added update conf slides and some work on envoy sample

---
+++ Web_Envoy/Client/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Sensorsystem;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var randomizer = new Random(); 
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new SensorService.SensorServiceClient(channel);
+
+            Console.WriteLine("Please type the device ID:");
+            var line = Console.ReadLine();
+
+            int sensorId = 0;
+            int.TryParse(line, out sensorId);
+
+
+            using (var cli = client.StreamData())
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (await cli.ResponseStream.MoveNext(cancellationToken: CancellationToken.None))
+                    {
+                        var response = cli.ResponseStream.Current;
+                        Console.WriteLine($"Receiving data from Sensor {response.SensorID} sent data1 {response.Data1}, data2 {response.Data2}");
+                    }
+                });
+
+
+                Console.WriteLine("Press any key to send random data or Q to stop.");
+
+                while ((line = Console.ReadLine()) != null)
+                {
+                    if (line.ToLower() == "q")
+                    {
+                        break;
+                    }
+                        
+                    int data1 = randomizer.Next(1, 100), data2 = randomizer.Next(1, 100);
+                    await cli.RequestStream.WriteAsync(new SensorData { SensorID = sensorId, Data1 = data1.ToString(), Data2 = data2});
+                    Console.WriteLine($"Sending data Sensor {sensorId} data1 {data1}, data2 {data2}");
+                }
+            }
+
+            await channel.ShutdownAsync();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ Web_Envoy/Server/Services/SensorHub.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Sensorsystem;
+
+namespace Server
+{
+    public class SensorHub : SensorService.SensorServiceBase
+    {
+        private readonly ILogger<SensorHub> _logger;
+        private static Random randomizer = new Random();
+        public SensorHub(ILogger<SensorHub> logger)
+        {
+            _logger = logger;
+
+            
+        }
+
+        public override Task<AvailableSensorsResponse> GetAvailableSensors(AvailableSensorsRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"received a GetAvailableSensors request from user {request.Username} message: {request.Message}");
+
+            return Task.FromResult(new AvailableSensorsResponse
+            {
+                Message = "Hello from server",
+                Devices = "This is the server replying with a list of devices"
+            });
+        }
+
+        public override async Task<SensorDataResponse> SendSensorData(IAsyncStreamReader<SensorData> requestStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"received some data from client Data1: {message.Data1}, Data2: {message.Data2}");
+            }
+
+            return new SensorDataResponse { Message = "Streamed data received correctly" };
+        }
+
+        public override async Task<Empty> SendSensorDataNoResponse(IAsyncStreamReader<SensorData> requestStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"received some data from client Data1: {message.Data1}, Data2: {message.Data2}");
+            }
+
+            return new Empty();
+        }
+
+        public override async Task ReceiveTemperatureUpdates(TemperatureRequest request, IServerStreamWriter<TemperatureData> responseStream, ServerCallContext context)
+        {
+            _logger.LogInformation($"received request to start stream temperature data from device id: {request.Deviceid}");
+
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Sending temperature data to device id: {request.Deviceid}.");
+
+                await responseStream.WriteAsync(new TemperatureData { Devicelocation = $"Random location {randomizer.Next(1, 100)}", Temperature = randomizer.Next(1, 100) });
+
+                await Task.Delay(500);
+            }
+
+            _logger.LogInformation($"stop streaming temperature data to device id: {request.Deviceid}");
+        }
+
+        private static ConcurrentDictionary<int, IServerStreamWriter<SensorData>> broadcastQueue = new ConcurrentDictionary<int, IServerStreamWriter<SensorData>>();
+
+
+        public override async Task StreamData(IAsyncStreamReader<SensorData> requestStream, IServerStreamWriter<SensorData> responseStream, ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext()) return;
+
+            do
+            {
+                broadcastQueue.TryAdd(requestStream.Current.SensorID, responseStream);
+                await BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+            
+            //broadcastQueue.TryRemove(context.Peer, out var _);
+        }
+
+        public async Task BroadcastMessageAsync(SensorData message)
+        {
+            foreach (var item in broadcastQueue.Where(x => x.Key != message.SensorID))
+            {
+                try
+                {
+                    _logger.LogInformation("Sending message", message);
+                    await item.Value.WriteAsync(message);
+                    //broadcastQueue.TryRemove(message.SensorID, out var _);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, ex);
+                    broadcastQueue.TryRemove(message.SensorID, out var _);
+                }
+            }
+        }
+
+        
+
+    }
+}
